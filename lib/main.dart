3a87e5186d3b1be84071db5a6b783f13a6e1a144
huisheng <huisheng@ximnet.com.my>
2022-09-11 16:06:52 +0800

    Aded login text fields

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,54 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            decoration: const InputDecoration(hintText: 'Enter email'),
+          ),
+          TextField(
+            controller: _password,
+            decoration: const InputDecoration(hintText: 'Enter Password'),
+          ),
+          TextButton(
+            onPressed: () async {},
+            child: const Text('Register'),
+          ),
+        ],
+      ),
+    );
   }
 }
