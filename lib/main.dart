b7fb4f58fdd2dcebfdff6434e56c3dd0aac00e6d
huisheng <huisheng@ximnet.com.my>
2022-09-12 23:33:44 +0800

    Add Firebase Import in login_view

--- lib/main.dart
+++ lib/main.dart
@@ -11,90 +11,20 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: const HomePage(),
+    home: const RegisterView(),
   ));
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<RegisterView> createState() => _RegisterViewState();
 }
 
-class _HomePageState extends State<HomePage> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
-
+class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Register"),
-      ),
-      body: FutureBuilder(
-          future: Firebase.initializeApp(
-            options: DefaultFirebaseOptions.currentPlatform,
-          ),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                return Column(
-                  children: [
-                    TextField(
-                      controller: _email,
-                      keyboardType: TextInputType.emailAddress,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      decoration:
-                          const InputDecoration(hintText: 'Enter email'),
-                    ),
-                    TextField(
-                      controller: _password,
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      decoration:
-                          const InputDecoration(hintText: 'Enter Password'),
-                    ),
-                    TextButton(
-                      onPressed: () async {
-                        // await Firebase.initializeApp(
-                        //   options: DefaultFirebaseOptions.currentPlatform,
-                        // );
-
-                        final email = _email.text;
-                        final password = _password.text;
-
-                        final userCredential = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: email, password: password);
-                        debugPrint(userCredential.toString());
-                      },
-                      child: const Text('Register'),
-                    ),
-                  ],
-                );
-              // break;
-              default:
-                return const Text('Loading...');
-            }
-          }),
-    );
+    return Container();
   }
 }
