03d02b4090b564e84ce3384e13eec2b6f9354f55
huisheng <huisheng@ximnet.com.my>
2022-09-12 23:30:29 +0800

    Add login view

---
+++ lib/views/login_view.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return Column(
+                  children: [
+                    TextField(
+                      controller: _email,
+                      keyboardType: TextInputType.emailAddress,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration:
+                          const InputDecoration(hintText: 'Enter email'),
+                    ),
+                    TextField(
+                      controller: _password,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration:
+                          const InputDecoration(hintText: 'Enter Password'),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        // await Firebase.initializeApp(
+                        //   options: DefaultFirebaseOptions.currentPlatform,
+                        // );
+
+                        final email = _email.text;
+                        final password = _password.text;
+
+                        final userCredential = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: email, password: password);
+                        debugPrint(userCredential.toString());
+                      },
+                      child: const Text('Register'),
+                    ),
+                  ],
+                );
+              // break;
+              default:
+                return const Text('Loading...');
+            }
+          }),
+    );
+  }
+}
