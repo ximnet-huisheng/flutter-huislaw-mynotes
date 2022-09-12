b5a56c497b0593eee5375f889cd7cd54336e54e1
huisheng <huisheng@ximnet.com.my>
2022-09-12 23:39:31 +0800

    Move register code to its own widget

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,78 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return Column(
+                  children: [
+                    TextField(
+                      controller: _email,
+                      keyboardType: TextInputType.emailAddress,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration:
+                          const InputDecoration(hintText: 'Enter email'),
+                    ),
+                    TextField(
+                      controller: _password,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration:
+                          const InputDecoration(hintText: 'Enter Password'),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        // await Firebase.initializeApp(
+                        //   options: DefaultFirebaseOptions.currentPlatform,
+                        // );
+
+                        final email = _email.text;
+                        final password = _password.text;
+
+                        final userCredential = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: email, password: password);
+                        debugPrint(userCredential.toString());
+                      },
+                      child: const Text('Register'),
+                    ),
+                  ],
+                );
+              // break;
+              default:
+                return const Text('Loading...');
+            }
+          }),
+    );
   }
 }
